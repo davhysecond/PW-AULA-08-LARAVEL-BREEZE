661caee2f62fc7a892912ed1bbc9f9035f6c35c2
Davhy Andrade <david.souza95@etec.sp.gov.br>
2023-06-07 01:08:11 -0300

    add: change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,7 +40,7 @@
 		<!-- Estado -->
 		<div>
 			<x-input-label for="estado" :value="__('Estado')" />
-			<x-select id="estado" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" name="estado"  required autofocus autocomplete="cidade"  >
+			<x-select id="estado" class="block mt-1 w-full" name="estado"  required autofocus autocomplete="cidade"  >
 					<option selected disabled hidden>{{ __('Escolha uma opção') }}</option>
 					@foreach ($states as $state)
 						<option>{{ $state}}</option>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false])
 
-<select {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
     {{ $slot }}
 </select>
